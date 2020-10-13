64ddb369ef2f66d012ebb58fe26d90e8bf0b281f
Connor Blakeney <connorblakeney@yahoo.com>
2020-10-13 10:23:31 -0500

    inserted data into tags

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Entries` (
 	`concept`	TEXT NOT NULL
 	`entry`	TEXT NOT NULL
 	`mood_id`	INTEGER NOT NULL
-    FOREIGN KEY(`mood_id`) REFERENCES`Moods`(`id`))
+    FOREIGN KEY(`mood_id`) REFERENCES`Moods`(`id`)
 );
 
 CREATE TABLE `Moods` (
@@ -24,5 +24,20 @@ CREATE TABLE `EntryTags` (
 	`tag_id`	INTEGER NOT NULL,
 	FOREIGN KEY(`entry_id`) REFERENCES `Entries`(`id`)
 	FOREIGN KEY(`tag_id`) REFERENCES `Tags`(`id`)
-
 );
+
+INSERT INTO `Entries` VALUES (null, '10/8/2020', "Python", "Started learning Python, can't wait for server side!", "4");
+INSERT INTO `Entries` VALUES (null, '10/13/2020', "SQL", "Learning SQL to complement Python", "5");
+
+INSERT INTO `Moods` VALUES (null, 'Clap Along With Pharrell');
+INSERT INTO `Moods` VALUES (null, 'Okay');
+INSERT INTO `Moods` VALUES (null, 'Unimpressed Mckayla Maroney');
+INSERT INTO `Moods` VALUES (null, 'Crying Jordan');
+INSERT INTO `Moods` VALUES (null, 'Alert');
+
+INSERT INTO `Tags` VALUES (null, 'Python');
+INSERT INTO `Tags` VALUES (null, 'SQL');
+
+INSERT INTO `EntryTags` VALUES (null, 1, 1);
+INSERT INTO `EntryTags` VALUES (null, 2, 2);
+
